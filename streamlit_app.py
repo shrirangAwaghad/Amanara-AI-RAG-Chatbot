ae7501d48eec375b66df3045e533f01549d92817
Shrirang Awaghad <shriawaghad@gmail.com>
2026-07-23 02:19:20 +0530

    Fix Streamlit import path

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# Add the src directory to Python's module search path
+sys.path.insert(0, str(Path(__file__).parent / "src"))
+
 from amanara.ui.app import app
 
 app()
